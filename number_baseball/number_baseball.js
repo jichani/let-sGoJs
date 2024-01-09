351c911d0e4f52823f044db9d883f81608c374de
Jichani <123962013+jichani@users.noreply.github.com>
2024-01-09 23:21:02 +0900

    숫자 야구 구현중

---
+++ number_baseball/number_baseball.js
@@ -0,0 +1,17 @@
+const $input = document.querySelector('#input');
+const $form = document.querySelector('#form');
+const $logs = document.querySelector('#logs');
+
+const numbers = []; // [1, 2, 3, 4, 5, 6, 7, 8, 9]
+for (let n = 0; n < 9; n += 1) {
+  numbers.push(n + 1);
+}
+
+const answer = [];
+for (let n = 0; n < 4; n += 1) { // 4번 반복
+  const index = Math.floor(Math.random() * numbers.length); // 0 ~ 8 정수
+  answer.push(numbers[index]);
+  numbers.splice(index, 1);
+}
+
+console.log(answer);
